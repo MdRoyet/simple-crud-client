aa82d355e037960ed7692451d2785aff51ccbd1d
Md Royet <rroyet8@gmail.com>
2026-05-10 20:32:26 +0600

    feat: add Heroui dependencies and update styles
    - Added @heroui/react and @heroui/styles to dependencies in package.json.
    - Replaced custom CSS variables and styles in globals.css with @heroui/styles import.
    - Updated the Home component in page.js to display a new heading.
    - Created a new users management page in src/app/users/page.jsx.

---
+++ src/app/users/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const usersPage = () => {
+  return (
+    <div className="items-center justify-center flex container mx-auto">
+      Users Management
+    </div>
+  );
+};
+
+export default usersPage;
